ace99a592d33414dbeaa259d47ff1a3887f205cc
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-05 14:48:41 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(Sysem.in);
+        Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         System.out.println(input + ": command not found");
     }
