3bf2966f984e8604d7f336de4b4253f01766136d
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-06 16:14:47 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import command.Commands;
 
 public class Main {
+
+    private static final Map<String, Consumer<String[]>> commads = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
+        Commands cmds = new Commands();
+        
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            String[] cmds = input.split(" ");
-
-            if (cmds[0].equals("exit")) return;
-            else if (cmds[0].equals("echo")) {
-                String output = String.join(" ", Arrays.copyOfRange(cmds, 1, cmds.length));
-                System.out.println(output);
-            }
-            else System.out.println(input + ": command not found");
+            cmds.act(input);
         }
         
     }
+    
 }
---
+++ src/main/java/command/Commands.java
@@ -0,0 +1,38 @@
+package command;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class Commands {
+
+	private final Map<String, Consumer<String[]>> commands = new HashMap<>();
+	
+	public void init() {
+		commands.put("exit", (param) -> System.exit(0));
+		commands.put("echo", (param) -> {
+			String output = String.join(" ", Arrays.copyOfRange(param, 1, param.length));
+            System.out.println(output);
+		});
+		commands.put("type", (param) -> {
+			String builtinCmd = param[1];
+			
+			if (commands.containsKey(builtinCmd)) {
+				System.out.println(builtinCmd + ": not found");
+				return;
+			}
+			System.out.println(builtinCmd + " is a shell builtin");
+		});
+	}
+	
+	public void act(String input) {
+		String[] cmd = input.split(" ");
+		Consumer<String[]> action = commands.getOrDefault(cmd[0], this::notFound);
+		action.accept(cmd);
+	}
+	
+	public void notFound(String[] param) {
+		System.out.println(param[0] + ": command not found");
+	}
+}
