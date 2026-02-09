ce9a3add9385d59e7982db7c51ee825afcf6481d
gyunho <gyunh01@pukyong.ac.kr>
2026-02-09 23:56:28 +0900

    feat: cd를 통해 절대 경로의 위치로 이동한다

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Consumer;
 
 import command.Commands;
 
 public class Main {
-
-    private static final Map<String, Consumer<String[]>> commads = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
